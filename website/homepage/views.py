b35feb78cea83abbf573fc17fabc13deecb770f0
Jimmy Sunbury <sunjimmy@comcast.net>
2024-04-11 13:52:14 -0400

    Removed poll app and added basic homepage for website.

---
+++ website/homepage/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+'''
+def index(request):
+    return HttpResponse("Welcome to the homepage!")
+'''
+
+
+def index(request):
+    return render(request, "homepage.html")
+
+def test(request):
+    return HttpResponseRedirect(reverse('polls:results', args=()))
