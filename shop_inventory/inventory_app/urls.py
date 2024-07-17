d2209c5599ccc4bec0fa67cc84c80d9cdcace28b
nbovee <12849851+nbovee@users.noreply.github.com>
2024-07-16 22:56:09 -0400

    drop dupes

--- shop_inventory/inventory_app/urls.py
+++
@@ -1,9 +0,0 @@
-# inventory_app/inventory_app/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('inventory.urls')),  # Include the app's URL configurations
-]
-
