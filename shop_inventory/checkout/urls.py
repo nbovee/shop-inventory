a7169fa09e3577a50e2422da44d8c00e222c7d1a
Bovee, Nicholas Joseph <bovee@rowan.edu>
2024-11-20 19:34:51 -0500

    enable checkout

--- shop_inventory/checkout/urls.py
+++ shop_inventory/checkout/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-app_name = "checkout"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="checkout"),
 ]
