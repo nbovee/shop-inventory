e8c7ad4b9958b2480e39a60040201644eefe1931
nbovee <12849851+nbovee@users.noreply.github.com>
2024-11-26 01:02:22 -0500

    adjustment to login redirects

--- shop-inventory/_core/views.py
+++ shop-inventory/_core/views.py
@@ -19,7 +19,8 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect("index")
+            next_url = request.GET.get("next", "index")
+            return redirect(next_url)
         else:
             messages.error(request, "Invalid username or password")
     return render(request, "core/login.html")
