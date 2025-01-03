e07356c6ba1937ac07800e02f649ab6546162990
nbovee <12849851+nbovee@users.noreply.github.com>
2025-01-02 22:00:15 -0500

    revise pi-gen

--- pi-gen-pantry/00-shop/02-run.sh
+++ pi-gen-pantry/00-shop/02-run.sh
@@ -1,34 +1,20 @@
 #!/bin/bash -e
 
+# Export environment variables for chroot
+export SHOP_INSTALL_DIR
+export SHOP_DJANGO_SUPERUSER_USERNAME SHOP_DJANGO_SUPERUSER_PASSWORD SHOP_DJANGO_SUPERUSER_EMAIL
+
 on_chroot << EOF
 # Collect static files
-cd /opt/shop-inventory
-/opt/shop-inventory/venv/bin/python manage.py collectstatic --noinput
+cd ${SHOP_INSTALL_DIR}
+${SHOP_INSTALL_DIR}/venv/bin/python manage.py collectstatic --noinput
 
 # Create initial database
-cd /opt/shop-inventory
-/opt/shop-inventory/venv/bin/python manage.py migrate --noinput
+${SHOP_INSTALL_DIR}/venv/bin/python manage.py migrate --noinput
+
+# Create superuser
+${SHOP_INSTALL_DIR}/venv/bin/python manage.py createsuperuser --noinput
 
 # Enable services to start on boot
 systemctl enable nginx
-systemctl enable shop-inventory
-
-# Create a first-boot service to generate a new secret key
-cat > /etc/systemd/system/shop-inventory-firstboot.service << 'EOL'
-[Unit]
-Description=First boot configuration for Shop Inventory
-Before=shop-inventory.service
-After=network.target
-
-[Service]
-Type=oneshot
-ExecStart=/bin/bash -c 'SECRET_KEY=$(openssl rand -base64 32) && sed -i "s/default-key-change-on-first-boot/$SECRET_KEY/" /opt/shop-inventory/.env'
-RemainAfter=no
-
-[Install]
-WantedBy=multi-user.target
-EOL
-
-# Enable first boot service
-systemctl enable shop-inventory-firstboot
 EOF
--- pi-gen-pantry/00-shop/03-install-service.sh
+++
@@ -1,5 +0,0 @@
-install -m 644 files/shop-inventory.service "${ROOTFS_DIR}/lib/systemd/system/"
-
-on_chroot << EOF
-systemctl enable shop-inventory
-EOF
