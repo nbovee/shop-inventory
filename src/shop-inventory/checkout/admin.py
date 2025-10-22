e9c1b26ec597cc4a493e136a467b60a7dd7c915b
nbovee <12849851+nbovee@users.noreply.github.com>
2025-10-22 00:07:43 -0400

    patch: improve admin display

--- src/shop-inventory/checkout/admin.py
+++ src/shop-inventory/checkout/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 from .models import Order, OrderItem
 
-# Register your models here.
-admin.site.register((Order, OrderItem))
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    raw_id_fields = ("inventory_item",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("order_number", "implicit_id", "date", "item_count")
+    list_filter = ("date",)
+    search_fields = ("order_number", "implicit_id")
+    readonly_fields = ("order_number", "date")
+    inlines = [OrderItemInline]
+
+    @admin.display(description="Items")
+    def item_count(self, obj):
+        """Display the number of items in this order."""
+        return obj.items.count()
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "inventory_item", "quantity")
+    list_filter = ("order__date",)
+    search_fields = ("order__order_number", "inventory_item__product__name")
+    raw_id_fields = ("order", "inventory_item")
--- src/shop-inventory/inventory/admin.py
+++ src/shop-inventory/inventory/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Product, Location, Inventory
 
-# Register your models here.
-admin.site.register((Product, Location, Inventory))
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "manufacturer", "barcode", "normalized_barcode", "active")
+    list_filter = ("active",)
+    search_fields = ("name", "manufacturer", "barcode", "normalized_barcode")
+    readonly_fields = ("normalized_barcode",)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("name", "active")
+    list_filter = ("active",)
+    search_fields = ("name",)
+
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ("product", "location", "quantity")
+    list_filter = ("location", "product__active")
+    search_fields = ("product__name", "product__manufacturer", "location__name")
+    raw_id_fields = ("product", "location")
